561106bafe172d09a936cfca7aaf3ff2791753db
foye6890 <foye6890aws@gmail.com>
2026-03-31 11:08:01 +0900

    first commit

---
+++ src/main/java/com/security/demo/SecurityDemoOidcApplication.java
@@ -0,0 +1,13 @@
+package com.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoOidcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoOidcApplication.class, args);
+	}
+
+}
